e18091267cbc2cd708a3b21243ec7a0f46186157
Garrett Weaver <wagarrettweaver@gmail.com>
2026-02-10 08:15:01 -0700

    Made tensor-run.py take input from stdin.

--- tensor-run.py
+++ tensor-run.py
@@ -4,12 +4,55 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import Model, load_model
 import sys
-from os import walk
+import os 
 import cv2
 import numpy as np
+from pathlib import Path
+import fileinput
+
+PROGRAM_NAME=str(sys.argv[0].lstrip('.').lstrip('/'))
+
+def runModel(model, inputImage):
+    file_path = Path(inputImage)
+
+    if file_path.exists():
+        # Load in the image from inputs from stdin 
+        im = cv2.imread(inputImage, cv2.COLOR_BGR2RGB) 
+
+        # We want to set the input image to the size of the images the network was 
+        # trained on 
+        dsize=(resize_width, resize_height)
+        im = cv2.resize(im, dsize) 
+
+        print(PROGRAM_NAME + ": info: It takes a long time to load the model...", file=sys.stderr)
+
+        image_in_array = np.vstack([[im]])
+
+        # Evaluate the model
+        result = model.predict(image_in_array[:1])
+        print(PROGRAM_NAME + "Result: " + str(result), file=sys.stderr)
+
+        index=0
+        indexOfHighestValue=-1
+        highestValue=0
+        # find index of the greatest value
+        for probability in result[0]:
+            if probability > highestValue:
+                indexOfHighestValue = index
+                highestValue = probability
+            index = index + 1
+
+        print(PROGRAM_NAME + ": info: indexOfHighestValue: " + str(indexOfHighestValue), file=sys.stderr)
+        print(PROGRAM_NAME + ": info: size of classesArray: " + str(len(classesArray)), file=sys.stderr)
+        if indexOfHighestValue >= 0:
+            print("\"" + classesArray[indexOfHighestValue] + "\" identified")
+        else:
+            print("Could not identify tree")
+    else:
+        print(PROGRAM_NAME + ": warning: Could not find image: \'" + str(inputImage) + "\'", file=sys.stderr)
 
 TrainingSetPath="Labels"
-indexRecord="classes.txt"
+indexRecord="classes.txt.tmp"
 
 resize_width=1000
 resize_height=562
@@ -35,14 +78,10 @@ print(tf.__version__)
 
 classesArray = []
 
-if len(sys.argv) < 2:
-    print (sys.argv[0] + ": error: positional input parameter 1 should be the path to an image.")
-    sys.exit(1)
-
-inputImage = sys.argv[1]
+inputImages = []
 
 # Read in the classes file, which contains a record of the trees we trained on
-file_path = TrainingSetPath + "/" + indexRecord
+file_path = indexRecord
 try:
     with open(file_path, 'r') as file:
         for line in file:
@@ -51,46 +90,26 @@ try:
             # You can process each line here.
             classesArray.append(line.strip())  # .strip() removes leading/trailing whitespace, including newlines
 except FileNotFoundError:
-    print(f"Error: The file '{file_path}' was not found.")
+    print(PROGRAM_NAME + ": error: The file '{file_path}' was not found.", file=sys.stderr)
     sys.exit(1)
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(PROGRAM_NAME + ": error: An error occurred: {e}", file=sys.stderr)
     sys.exit(1)
-
-# Load in the image from inputs from stdin 
-im = cv2.imread(inputImage, cv2.COLOR_BGR2RGB) 
-
-# We want to set the input image to the size of the images the network was 
-# trained on 
-dsize=(resize_width, resize_height)
-im = cv2.resize(im, dsize) 
-
-print("It takes a long time to load the model...")
 
 model = tf.keras.models.load_model(checkpoint_path)
 if model is not None:
-    print(sys.argv[0] + ": info: Model loaded successfully!")
+    print(PROGRAM_NAME + ": info: Model loaded successfully!", file=sys.stderr)
 else:
-    print(sys.argv[0] + ": error: Failed to load the model.")
+    print(PROGRAM_NAME + ": error: Failed to load the model.", file=sys.stderr)
     sys.exit(1)
 
-image_in_array = np.vstack([[im]])
-
-# Evaluate the model
-result = model.predict(image_in_array[:1])
-print("Result: " + str(result))
-
-index=0
-indexOfHighestValue=-1
-highestValue=0
-# find index of the greatest value
-for probability in result[0]:
-    if probability > highestValue:
-        indexOfHighestValue = index
-    index = index + 1
-
-if indexOfHighestValue >= 0:
-    print("\"" + classesArray[indexOfHighestValue] + "\" identified")
+# If no positional parameters are provided, take input from stdin
+if len(sys.argv) < 2:
+    for inputImage in fileinput.input():
+        runModel(model, inputImage.rstrip())
 else:
-    print("Could not identify tree")
+    inputImages = sys.argv[1:]
+    for inputImage in inputImages:
+        runModel(model, inputImage)
 
+        
