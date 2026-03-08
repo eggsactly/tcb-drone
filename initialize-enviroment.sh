84bbb8a935df8b0df1886fcdd744706eed489ece
Garrett Weaver <wagarrettweaver@gmail.com>
2026-03-08 16:52:59 -0700

    Checked python version in initialize-enviroment.sh

--- initialize-enviroment.sh
+++ initialize-enviroment.sh
@@ -19,11 +19,34 @@ TRAINING_SET=(Parker.tar.gz RillitoPark CherryPark)
 IS_INSTALLED=0
 tf_ver=""
 
+readonly HAS_PYTHON=$(which python3 | wc -l)
+
+if [ ${HAS_PYTHON} -eq 0 ]; then 
+    >&2 printf "${0}: Error: python not found.\n"
+    >&2 printf "${0}: Info: please install python3 and try again.\n"
+    exit 1
+fi
+
+PYTHON_MAJOR=$(python3 --version | grep -Po '[0-9]+\.[0-9]+' | awk '{split($1,a,".");print a[1]}')
+PYTHON_MINOR=$(python3 --version | grep -Po '[0-9]+\.[0-9]+' | awk '{split($1,a,".");print a[2]}')
+
+if [ ${PYTHON_MAJOR} -ne 3 ]; then 
+    >&2 printf "${0}: Error: python version needs to be 3, your version is: %d.\n" ${PYTHON_MAJOR}
+    >&2 printf "${0}: Info: please install python3 and try again.\n" 
+    exit 1
+fi
+
+if [ ${PYTHON_MINOR} -le 10 ]; then 
+    >&2 printf "${0}: Error: python version needs to be 3.10, or higher, your version is: 3.%d.\n" ${PYTHON_MINOR}
+    >&2 printf "${0}: Info: please install python3.%d and try again.\n" ${PYTHON_MINOR}
+    exit 1
+fi
+
 if [[ $1 == "GPU" ]]; then
     tf_ver="tensorflow[and-cuda]"
     check_tf_installed () {
         # Check that a GPU device is listed
-        GPU_DEVICE=$(python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))" | grep -Po "\[PhysicalDevice\(.*\)\]")
+        GPU_DEVICE=$(python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))" | grep -Po "\[PhysicalDevice\(.*\)\]")
         echo "GPU Device Found: ${GPU_DEVICE}"
         if [[ -n "${GPU_DEVICE}" ]]; then
             IS_INSTALLED=1
@@ -33,7 +56,7 @@ else
     tf_ver="tensorflow"
     check_tf_installed () {
        # Verify installation 
-        IS_INSTALLED=$(python -c "import tensorflow as tf; print(tf.__version__)"  | grep -Po "[0-9]+\.[0-9]+\.[0-9]+"  | wc -l)
+        IS_INSTALLED=$(python3 -c "import tensorflow as tf; print(tf.__version__)"  | grep -Po "[0-9]+\.[0-9]+\.[0-9]+"  | wc -l)
     }
 fi
 
@@ -43,7 +66,7 @@ if [ -f tensorflow/bin/activate  ]; then
 fi
 
 # If not already installed, get tensor flow 
-if [[ $IS_INSTALLED -eq 0 ]]; then
+if [[ ${IS_INSTALLED} -eq 0 ]]; then
     # Delete the tensorflow folder if one exists and is not a verified install 
     rm -rf tensorflow/
     # Get tensorflow (CPU Version) 
@@ -59,7 +82,7 @@ if [[ $IS_INSTALLED -eq 0 ]]; then
     # Verify installation 
     check_tf_installed
 
-    if [[ $IS_INSTALLED -gt 0 ]]; then
+    if [[ ${IS_INSTALLED} -gt 0 ]]; then
         echo "Installation Successful"
     else
         echo "Installation unsuccessful"
